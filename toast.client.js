b4586540cc2f3aa44f257c384251a888a4ffb72e
Gabriel Chavez <gabosvs@gmail.com>
2023-07-21 20:42:31 -0600

    fix issues

--- toast.client.js
+++ toast.client.js
@@ -15,22 +15,28 @@ export default (ctx, inject) => {
 
   const ToastProgrammatic = {
     show (props) {
+      document.getElementById("toasts").classList.remove('hidden')
       if (typeof props === 'string') props = { message: props }
       return spawn('toasts', props, TvToast, Vue, options)
     },
     success (message) {
+      document.getElementById("toasts").classList.remove('hidden')
       return spawn('toasts', {type: 'success', message}, TvToast, Vue, options)
     },
     info (message) {
+      document.getElementById("toasts").classList.remove('hidden')
       return spawn('toasts', {type: 'info', message}, TvToast, Vue, options)
     },
     danger (message) {
+      document.getElementById("toasts").classList.remove('hidden')
       return spawn('toasts', {type: 'danger', message, timeout: 20}, TvToast, Vue, options)
     },
     warning (message) {
+      document.getElementById("toasts").classList.remove('hidden')
       return spawn('toasts', {type: 'warning', message, timeout: 10}, TvToast, Vue, options)
     },
     denied (message) {
+      document.getElementById("toasts").classList.remove('hidden')
       return spawn('toasts', {type: 'denied', message, timeout: 10}, TvToast, Vue, options)
     },
   }
